2657c7c5c089c4b0f14654ac244bced0e91021d5
Maksim Torzhkov <maxtorj@yandex.ru>
2025-03-27 13:28:57 +0300

    add history test case

--- src/Main.java
+++ src/Main.java
@@ -8,65 +8,62 @@ public class Main {
         tm.addTask(task1);
         Task task2 = new Task("Task 2", "Description 2", Status.IN_PROGRESS);
         tm.addTask(task2);
-        System.out.println(tm.getAllTasks());
 
         Epic epic1 = new Epic("Epic 1", "Description 1");
         tm.addEpic(epic1);
         tm.updateEpicStatus(epic1.getId());
-        System.out.println(tm.getAllEpics());
         Subtask sub1 = new Subtask("Subtask 1", "Description 1", Status.NEW,  epic1.getId());
         tm.addSubtask(sub1);
         Subtask sub2 = new Subtask("Subtask 2", "Description 2", Status.IN_PROGRESS, epic1.getId());
         tm.addSubtask(sub2);
-        System.out.println(tm.getAllSubtasks());
 
         Epic epic2 = new Epic("Epic 2", "Description 2");
         tm.addEpic(epic2);
         tm.updateEpicStatus(epic2.getId());
-        System.out.println(tm.getAllEpics());
         Subtask sub3 = new Subtask("Subtask 3", "Description 3", Status.IN_PROGRESS, epic2.getId());
         tm.addSubtask(sub3);
-        System.out.println(tm.getAllSubtasks());
 
         task1.setStatus(Status.IN_PROGRESS);
         tm.updateTask(task1);
-        System.out.println(tm.getAllTasks());
-
         sub1.setStatus(Status.DONE);
         sub2.setStatus(Status.DONE);
         tm.updateSubtask(sub1);
         tm.updateSubtask(sub2);
         sub3.setStatus(Status.IN_PROGRESS);
         tm.updateSubtask(sub3);
-        System.out.println(tm.getAllSubtasks());
-        System.out.println(tm.getAllEpics());
 
-        tm.deleteTaskById(1);
-        tm.deleteSubtaskById(4);
-        tm.deleteEpicById(6);
-
-        System.out.println(tm.getAllTasks());
-        System.out.println(tm.getAllSubtasks());
-        System.out.println(tm.getAllEpics());
-
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
-        tm.getTaskById(task2.getId());
+        tm.getTaskById(task1.getId());
         tm.getTaskById(task2.getId());
         tm.getEpicById(epic1.getId());
+        tm.getEpicById(epic2.getId());
+        tm.getSubtaskById(sub1.getId());
+        tm.getSubtaskById(sub2.getId());
+        tm.getSubtaskById(sub3.getId());
 
-        List<Task> history = tm.getHistory();
-        System.out.println("История содержит " + history.size() + " элементов.");
-        for (Task task : history) {
-            if (task != null) {
-            System.out.println("- " + task.getTitle());
+        printAllTasks(tm);
+    }
+
+    private static void printAllTasks(TaskManager manager) {
+        System.out.println("Задачи:");
+        for (Task task : manager.getAllTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("Эпики:");
+        for (Task epic : manager.getAllEpics()) {
+            System.out.println(epic);
+
+            for (Task task : manager.getAllSubtasksByEpicId(epic.getId())) {
+                System.out.println("--> " + task);
             }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : manager.getAllSubtasks()) {
+            System.out.println(subtask);
+        }
+
+        System.out.println("История:");
+        for (Task task : manager.getHistory()) {
+            System.out.println(task);
         }
     }
 }
