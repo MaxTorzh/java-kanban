ccd7c516ea98a0749a82b6555a47cbbbf2374520
Maksim Torzhkov <maxtorj@yandex.ru>
2025-03-28 22:00:29 +0300

    add SubtaskTest comments

--- test/SubtaskTest.java
+++ test/SubtaskTest.java
@@ -7,7 +7,7 @@ class SubtaskTest {
     void subtaskCanNotBeSelfEpic() {
         InMemoryTaskManager tm = new InMemoryTaskManager();
         Subtask subtask = new Subtask("Subtask", "Desc", Status.NEW, 1);
-        subtask.setId(1);
-        assertThrows(IllegalArgumentException.class, () -> tm.addSubtask(subtask));
-    }
-}
+        subtask.setId(1); // Устанавливаем конкретный id для проверки
+        assertThrows(IllegalArgumentException.class, () -> tm.addSubtask(subtask)); // Ожидаем исключение IllegalArgumentException
+    } // В методе InMemoryTaskManager.addSubtask() есть проверка
+} // Поскольку epicId = 1 и id = 1, условие выполняется → выбрасывается IllegalArgumentException
