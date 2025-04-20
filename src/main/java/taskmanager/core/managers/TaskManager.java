377f65bc7c723fc999191c310ec9dee9f11c571e
Maksim Torzhkov <maxtorj@yandex.ru>
2025-04-20 22:05:39 +0300

    feat: update code style

--- src/main/java/taskmanager/core/managers/TaskManager.java
+++ src/main/java/taskmanager/core/managers/TaskManager.java
@@ -4,32 +4,44 @@ import taskmanager.core.model.*;
 import java.util.List;
 
 public interface TaskManager {
-
     List<Task> getAllTasks();
+
     List<Subtask> getAllSubtasks();
+
     List<Epic> getAllEpics();
 
     void deleteAllTasks();
+
     void deleteAllSubtasks();
+
     void deleteAllEpics();
 
     Task getTaskById(int id);
+
     Subtask getSubtaskById(int id);
+
     Epic getEpicById(int id);
 
     List<Task> getHistory();
 
     void addTask(Task task);
+
     void addSubtask(Subtask subtask);
+
     void addEpic(Epic epic);
 
     void updateEpicStatus(int epicId);
+
     void updateTask(Task task);
+
     void updateSubtask(Subtask subtask);
+
     void updateEpic(Epic epic);
 
     void deleteTaskById(int id);
+
     void deleteSubtaskById(int id);
+
     void deleteEpicById(int id);
 
     List<Subtask> getAllSubtasksByEpicId(int epicId);
