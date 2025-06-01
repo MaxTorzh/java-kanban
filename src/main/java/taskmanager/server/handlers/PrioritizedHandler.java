ccd3a7c3341ff4f685ade9013b25c2bbec0f0265
Maksim Torzhkov <maxtorj@yandex.ru>
2025-06-01 10:23:13 +0300

    feat: add history and prioritized handlers

---
+++ src/main/java/taskmanager/server/handlers/PrioritizedHandler.java
@@ -0,0 +1,34 @@
+package taskmanager.server.handlers;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import taskmanager.core.managers.TaskManager;
+
+import java.io.IOException;
+import java.rmi.ServerError;
+
+/**
+ * Обработчик HTTP-запросов для получения отсортированного списка задач по приоритету.
+ * Поддерживает только GET /prioritized.
+ */
+public class PrioritizedHandler extends BaseHttpHandler {
+    private final Gson gson;
+
+    public PrioritizedHandler(TaskManager taskManager, Gson gson) {
+        super(taskManager);
+        this.gson = gson;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            if (!exchange.getRequestMethod().equals("GET")) {
+                sendText(exchange, "{\"error\":\"Метод не поддерживается\"}", 405);
+                return;
+            }
+            sendText(exchange, gson.toJson(taskManager.getPrioritizedTasks()), 200);
+        } catch (Exception e) {
+            sendServerError(exchange, e);
+        }
+    }
+}
